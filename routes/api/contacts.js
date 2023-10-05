7a333922346aa3f0419a03e9147695143c6dd3a8
=Andrei <balasca.andrei@gmail.com>
2023-10-05 20:10:24 +0300

    working

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 
 const router = express.Router();
 const { v4: uuidv4 } = require("uuid");
-const fs = require("fs/promises");
+const fs = require("fs");
 const path = require("path");
 
 const contactsPath = path.join(__dirname, "../../models/contacts.json");
